84b8966ddf41761f6c00d452b2961d50cd2765e4
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-02-07 10:16:47 -0600

    updated code for tests exercises

--- code.js
+++ code.js
@@ -4,5 +4,18 @@ function helloWorld() {
 }
 
 function sayHello(input){
-    return "Hello, " + input + "!";
+    if (input === undefined){
+        return "Hello, World!"
+    }else if(typeof input === "boolean"){
+        return "Hello, World!"
+    } else{
+    return "Hello, " + input + "!";}
+}
+
+function isFive(input){
+return input == 5;
+};
+
+function isEven(input){
+return input % 2 === 0
 }
