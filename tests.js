e3217ce083c241a455a58a76a8572b3c93944d04
Jeremy Sellers <jeremy.c.sellers@gmail.com>
2022-02-07 10:17:07 -0600

    updated tests code for continuing exercises

--- tests.js
+++ tests.js
@@ -28,7 +28,44 @@ describe("sayHello", function (){
         expect(sayHello("Alex")).toBe("Hello, Alex!");
     });
     it('should return the string "Hello, Pat! when executed', function () {
-        expect(sayHello("Pat")).toBe("Hello, Pat!")
-        
+        expect(sayHello("Pat")).toBe("Hello, Pat!");
+    });
+    it('should return the string "Hello, World! when executed', function () {
+        expect(sayHello()).toBe("Hello, World!");
+    });
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(sayHello(true)).toBe("Hello, World!");})
+    it('should return the string "Hello, World!" when executed with an input of false', function () {
+        expect(sayHello(false)).toBe("Hello, World!");
+        });
+});
+describe("isFive", function(){
+    it('should be a defined function', function (){
+        expect(typeof isFive).toBe("function")
+    }
+    );
+    it('should return true if input equals five', function () {expect(isFive(5)).toBe(true)
+    });
+})
+describe("isEven", function(){
+    it('should be a defined function', function () {expect(typeof isEven).toBe("function")
+    });
+    it('should return a boolean', function () {expect(typeof isEven()).toBe( "boolean")
+    });
+    it('should return true when passed 2', function () {expect(isEven(2)).toBe(true);
+    });
+    it('should return true when passed -4', function () {expect(isEven(-4)).toBe(true)
+    });
+    it('should return false when passed 3', function () {expect(isEven(3)).toBe(false)
+    });
+    it('should return false when passed "banana" ', function () {expect(isEven("banana")).toBe(false)
+    });
+    it('should return true when passed "8" ', function () {expect(isEven("8")).toBe(true);
+    });
+    it('should return false when called with infinity', function () {expect(isEven(Infinity)).toBe(false)
+    });
+    it('should return false when passed true or false', function () {expect(isEven(typeof "boolean")).toBe(false)
+    });
+    it('should return false when not given an argument', function () {expect(isEven()).toBe(false)
     });
 });
